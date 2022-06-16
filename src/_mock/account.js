002509fe7f158bb5a5937c3530ac522d3e0d55c0
itsraziee <itsrazieemaetan@gmail.com>
2022-06-16 16:26:01 +0800

    removing logo comments

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,9 +1,9 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+  displayName: 'Park Chanyeol',
+  email: 'pcy@gmail.com',
+  photoURL: 'https://i.pinimg.com/originals/5f/46/c9/5f46c9d4638932143cd332ca68663749.png',
 };
 
 export default account;
