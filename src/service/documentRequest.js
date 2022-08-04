42a8fc1f46ab3a38c84756a351bcbbd4b3b421a9
Mark Zhang <68852130+archaga@users.noreply.github.com>
2022-08-04 14:53:52 +0800

    fixed document request to firebase

--- src/service/documentRequest.js
+++ src/service/documentRequest.js
@@ -2,5 +2,5 @@ import { addDoc, collection } from 'firebase/firestore';
 import { firestore } from '../firebase-init';
 
 export function createRequest(type, data) {
-  return addDoc(collection(firestore, 'documentRequest'), data);
+  return addDoc(collection(firestore, 'documentRequest'), { type, data });
 }
