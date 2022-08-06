570d0c48d443b578d6c2a05b1ba9c9efc2f58155
Alvin Jay Auditor <96163941+Ajauditor@users.noreply.github.com>
2022-08-06 16:55:30 +0800

    buhatan sa lupong tagapamayapa

---
+++ src/sections/documents/BuhatanSaLupongTagapamayapa/index.js
@@ -0,0 +1,231 @@
+import { Box } from '@mui/material';
+import { fontWeight, style } from '@mui/system';
+import React from 'react';
+import styles from './BuhatanSaLupongTagapamayapa.module.css';
+
+export default function BuhatanSaLupongTagapamayapa() {
+  return (
+    <Box classNameName={styles.c15}>
+      <p className={styles.c7}>
+        <span className={styles.c1}>Republic of the Philippines</span>
+        <span
+          style={{
+            overflow: 'hidden',
+            display: 'inline - block',
+            margin: '0.00px 0.00px',
+            border: '0.00px solid #000000',
+            transform: 'rotate(0.00rad) translateZ(0px)',
+            WebkitTransform: 'rotate(0.00rad) translateZ(0px)',
+            width: '102.50px',
+            height: '100.23px',
+          }}
+        >
+          <img
+            alt=""
+            src="images/image1.png"
+            style={{
+              width: '102.50px',
+              height: '100.23px',
+              marginLeft: '0.00px',
+              marginTop: '0.00px',
+              transform: 'rotate(0.00rad) translateZ(0px)',
+              WebkitTransform: 'rotate(0.00rad) translateZ(0px)',
+            }}
+            title=""
+          />
+        </span>
+        <span
+          style={{
+            overflow: 'hidden',
+            display: 'inline - block',
+            margin: '0.00px 0.00px',
+            border: '0.00px',
+            solid: '#000000.toFixed',
+            transform: 'rotate(0.00rad) translateZ(0px)',
+            WebkitTransform: 'rotate(0.00rad) translateZ(0px)',
+            width: '103.00px',
+            height: '98.52px',
+          }}
+        >
+          <img
+            alt=""
+            src="images/image2.png"
+            style={{
+              width: '103.00px',
+              height: '98.52px',
+              marginLeft: '0.00px',
+              marginTop: '0.00px',
+              transform: 'rotate(0.00rad) translateZ(0px)',
+              WebkitTransform: 'rotate(0.00rad) translateZ(0px)',
+            }}
+            title=""
+          />
+        </span>
+      </p>
+      <p className={styles.c7}>
+        <span className={styles.c1}>Province of Bukidnon</span>
+      </p>
+      <p className={Styles.c7}>
+        <span className={styles.c1}>Municipality of Pangantucan</span>
+      </p>
+      <p className={styles.c7}>
+        <span className={styles.c1}>BARANGAY KIMANAIT</span>
+      </p>
+      <p className={(styles.c7, styles.c12)}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={(styles.c7, styles.c12)}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c7}>
+        <span className={styles.c3}>BUHATAN SA LUPONG TAGAPAMAYAPA</span>
+      </p>
+      <p className={(styles.c0, styles.c13)}>
+        <span className={styles.c3}></span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c4}>
+          ___________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{' '}
+        </span>
+        <span className={styles.c1}>Kaso sa Barangay Numero ________</span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>
+          _____________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Mahitungod sa _________________
+        </span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>
+          _____________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_____________________________
+        </span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>&nbsp; &nbsp; [Mga] Nagsumbong</span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <ul className={(styles.c16, (styles = 'lst-kix_rs7lkl8oocgz-0 start'))}>
+        <li className={(styles.c2, styles.c11, (styles = 'li-bullet-0'))}>
+          <span className={styles.c1}>batok -</span>
+        </li>
+      </ul>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>______________________</span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>______________________</span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>______________________</span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>&nbsp; &nbsp; &nbsp; &nbsp;[Mga] Sinumbong</span>
+      </p>
+      <p className={styles.c0}>
+        <span className={stylesc1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c14}>
+        <span className={styles.c3}>PAGTAWAG</span>
+      </p>
+      <p className={styles.c14}>
+        <span className={styles.c1}>[Summons]</span>
+      </p>
+      <p className={styles.c9}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>Ngadto kang: _____________________</span>
+      </p>
+      <p className={styles.c2}>
+        <span className={styles.c1}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___________________________</span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c14}>
+        <span className={styles.c1}>[Mga] Sinumbong</span>
+      </p>
+      <p className={styles.c9}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c10}>
+        <span className={styles.c4}>
+          Gitawag ikaw / kamo dinhi sapag-atubang personal kanako uban sa imong /inyong mga testigos, sa ika _____ nga
+          adlaw sa ____________ bulan sa _____________ sa alas _________ ang takna sa buntag / hapon, dinhi sa{' '}
+        </span>
+        <span className={styles.c4}>LUPONG TAGAPAMAYAPA OFFICE </span>
+        <span className={styles.c4}>LUPONG TAGAPAMAYAPA OFFICE </span>
+        <span className={styles.c1}>
+          ning barangay, aron sa pagtubag sa usa ka sumbong nga gihimo atubangan kanako [ang kopya sa maong sumbong
+          gilakip dinhi], alang sa malinawong paghusay sa inyong kasamok uban sa magsumbong.
+        </span>
+      </p>
+      <p className={styles.c8}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c10}>
+        <span className={styles.c1}>
+          Gipahimatngonan ikaw / kamo nga kung ikaw / kamo magdumili o motuyo sa pagpakyas sa pagpakita agi&rsquo;g
+          pagtuman niining pagtawag, mahimo nga ikaw / kamo did-an na sa pagpasaka sa bisan unsang sumbalik sumbong nga
+          mutuybo uban sa nagsumbong.
+        </span>
+      </p>
+      <p className={styles.c8}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c10}>
+        <span className={styles.c4}>Tumana kini o kon dili, atubanga ang silot &ldquo;</span>
+        <span className={styles.c4}>as for contempt of court</span>
+        <span className={styles.c1}>
+          &rdquo; kon susama nga silot nga ipahamtang sa hukmanan alang niadtong mosupak sa kamanduan niini.
+        </span>
+      </p>
+      <p className={styles.c8}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c10}>
+        <span className={styles.c1}>Niining ika ______ nga adlaw sa ______________________, _____.</span>
+      </p>
+      <p className={styles.c8}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c0}>
+        <span className={styles.c1}></span>
+      </p>
+      <p className={styles.c6}>
+        <span className={styles.c5}>JERRY P. PARADILLO</span>
+      </p>
+      <p className={styles.c6}>
+        <span className={styles.c1}>Punong Barangay</span>
+      </p>
+    </Box>
+  );
+}
