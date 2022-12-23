e53837333197525963ffe5ce5a89b14b19d08a96
itsraziee <itsrazieemaetan@gmail.com>
2022-12-23 16:02:34 +0800

    implemented auto logout on 5 minute inactivity

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Router from './routes';
 // theme
 import ThemeProvider from './theme';
 // components
+import IdleChecker from './components/IdleChecker';
 import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 import './firebase-init';
@@ -12,11 +13,13 @@ import './firebase-init';
 export default function App() {
   return (
     <SnackbarProvider preventDuplicate>
-      <ThemeProvider>
-        <ScrollToTop />
-        <BaseOptionChartStyle />
-        <Router />
-      </ThemeProvider>
+      <IdleChecker>
+        <ThemeProvider>
+          <ScrollToTop />
+          <BaseOptionChartStyle />
+          <Router />
+        </ThemeProvider>
+      </IdleChecker>
     </SnackbarProvider>
   );
 }
